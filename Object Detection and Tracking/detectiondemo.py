054b86a25a797ab66ee7dcae39785533e865b55c
SivaRakeshRelangi <sivarakesh993@gmail.com>
2022-10-19 09:32:39 +0530

    AI

---
+++ Object Detection and Tracking/detectiondemo.py
@@ -0,0 +1,70 @@
+# pylint: disable=E1101
+# pylint: disable=C0116
+# pylint: disable=C0103
+# pylint: disable=C0114
+# pylint: disable=C0200
+import cv2
+import numpy as np
+
+# Load Yolo
+net = cv2.dnn.readNet("cfg/yolov3.cfg", "cfg/yolov3.weights")
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+# Loading image
+img = cv2.imread("groupFriends.jpeg")
+img = cv2.resize(img, None, fx=0.4, fy=0.4)
+height, width, channels = img.shape
+
+# Detecting objects
+blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+net.setInput(blob)
+outs = net.forward(output_layers)
+
+# Showing informations on the screen
+class_ids = []
+confidences = []
+boxes = []
+for out in outs:
+    for detection in out:
+        scores = detection[5:]
+        class_id = np.argmax(scores)
+        confidence = scores[class_id]
+        if confidence > 0.5:
+            # Object detected
+            center_x = int(detection[0] * width)
+            center_y = int(detection[1] * height)
+            w = int(detection[2] * width)
+            h = int(detection[3] * height)
+
+            # Rectangle coordinates
+            x = int(center_x - w / 2)
+            y = int(center_y - h / 2)
+
+            boxes.append([x, y, w, h])
+            confidences.append(float(confidence))
+            class_ids.append(class_id)
+indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+font = cv2.FONT_HERSHEY_PLAIN
+for i in range(len(boxes)):
+    if i in indexes:
+        x, y, w, h = boxes[i]
+        label = str(classes[class_ids[i]])
+        color = colors[i]
+        cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+        cv2.putText(img, label, (x+30, y+20), font, 1, color, 1)
+
+scale_percent = 130 # percent of original size
+width = int(img.shape[1] * scale_percent / 100)
+height = int(img.shape[0] * scale_percent / 100)
+dim = (width, height)
+        # resize image
+resized = cv2.resize(img, dim, interpolation= cv2.INTER_AREA)
+cv2.imshow("Image", resized) 
+cv2.imshow("Image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
